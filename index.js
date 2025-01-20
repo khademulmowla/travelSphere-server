f4182ba0afd4fc268c48386baf35fd150c4d2c61
khademul mowla <khademulmowla31eee@gmail.com>
2025-01-21 02:53:52 +0600

    saved a package in db

--- index.js
+++ index.js
@@ -23,6 +23,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        const db = client.db('travelDb')
+        const packagesCollection = db.collection('packages')
+
+
+        // jwt related api //
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ token })
+        })
+
+        // middlewares //
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+
+        }
+
+        // save a package in db //
+        app.post('/packages', verifyToken, async (req, res) => {
+            const package = req.body
+            const result = await packagesCollection.insertOne(package)
+            res.send(result)
+        })
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
